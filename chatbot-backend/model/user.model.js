c44049d98fdac7ab584d16e2246a6e2da638e13d
Mohamed Yassine Khlaf <150388066+MEDyassKH3100@users.noreply.github.com>
2024-07-18 01:34:27 +0100

    Update user.model

--- chatbot-backend/model/user.model.js
+++ chatbot-backend/model/user.model.js
@@ -1,48 +1,39 @@
 const mongoose = require('mongoose');
 const db = require('../config/db');
 
-const{ Schema,model }=mongoose;
-const userSchema = new Schema ({
-    nom: {
-        type: String,
-        required: true
-      },
-      prenom: {
-        type: String,
-        required: true
-      },
+const { Schema, model } = mongoose;
 
-    cin:{
-        type: Number,
-        unique:true,
-        require:true
+const userSchema = new Schema({
+  nom: {
+    type: String,
+    required: true
+  },
+  prenom: {
+    type: String,
+    required: true
+  },
+  cin: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  identifiant: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  mdp: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
 
-    },
-
-    identifiant:{
-        type:String,
-        unique:true,
-        require:true
-
-    },
-
-    email:{
-        type:String,
-        lowercase:true,
-        unique:true,
-        require:true
-
-    },
-
-    mdp:{
-        type:String,
-        unique:true,
-        require:true
-
-    }
-
-    
-})
-
-const UserModel = db.model('user',userSchema);
-model.exports = UserModel;
+const UserModel = model('User', userSchema);
+module.exports = UserModel;
