aefc6c912c31851b317a4455bc6df91b83a6bdde
Mohamed Yassine Khlaf <150388066+MEDyassKH3100@users.noreply.github.com>
2024-08-09 01:04:33 +0100

    Attestation

---
+++ chatbot-backend/controller/attestation.controller.js
@@ -0,0 +1,1 @@
+const UserModel = require("../model/attestaion.model");
---
+++ chatbot-backend/model/attestation.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const db = require("../config/db");
+
+const { Schema } = mongoose;
+
+const attestaionSchema = new Schema({
+
+    
+
+})
